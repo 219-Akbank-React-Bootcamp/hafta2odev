2083c04446736e6944fec739284cc9f846d2aa33
hakanozoglu <hakan.ozoglu@grapecity.com>
2022-09-11 12:23:08 +0300

    changes

--- app.js
+++ app.js
@@ -1,39 +1,64 @@
-const getValuesFromForm = (formId)=>{
-    const f = document.getElementById(formId)
-    return Array.from(f.childNodes).filter(node=>node.tagName === "INPUT")
-    .reduce((prev,curr)=>{
-        prev[curr.name] = curr.value
-        return prev
-    },{})
-}
-const handleOnChangeText = (event,inputName)=>{
-    state[inputName]=event.target.value
-}
 
+const handleOnChangeText = (event, inputName) => {
+  state[inputName] = event.target.value
+  render()
+}
+const render = () => {
+  const root = document.getElementById('root')
+  root.innerHTML = App()
+}
 const state = {
-    inputAdSoyad:"",
-    inputBakiye:"",
-    userList: [
-        {id:1,adSoyad:"Hakan Özoğlu",bakiye:1000},
-        {id:2,adSoyad:"Pelin Hangişi",bakiye:2000},
-        {id:3,adSoyad:"Burcu Yılmaz",bakiye:500},
-        {id:4,adSoyad:"İlayda Yurttakalan",bakiye:1500},
-    ]
+  inputAdSoyad: '',
+  inputBakiye: '',
+  inputAmount: '',
+  inputTransactionSender: '1',
+  inputTransactionReceiver: '2',
+  userList: [
+    { id: '1', adSoyad: 'Hakan Özoğlu', bakiye: 1000 },
+    { id: '2', adSoyad: 'Pelin Hangişi', bakiye: 2000 },
+    { id: '3', adSoyad: 'Burcu Yılmaz', bakiye: 500 },
+    { id: '4', adSoyad: 'İlayda Yurttakalan', bakiye: 1500 },
+  ],
 }
 
-const App = ()=>{
-    return `<div class="container">
+const App = () => {
+  return `<div class="container">
     <div class="left">
-    <div class="addUserForm">${AddUserForm()}</div>
+    <div class="addUserForm">${AddUserForm()}
+    </div>
     <div class="userList">${UserList()}</div>
     </div>
-    <div class="center">a</div>
-    <div class="rigt">a</div>
+    <div class="center">
+    <div class="transactionForm">
+        ${TransactionForm()}
+    </div>
+    </div>
+    <div class="right">a</div>
     </div>`
 }
 
-const AddUserForm = ()=>{
-    return `<form class="addUserForm__form" id="addUserForm">
+const TransactionForm = () => {
+  return `<form>
+    ${UserSelectList({ name: 'sender',stateControl:"inputTransactionSender", value:`${state.inputTransactionSender}`})}
+    <input  value="${state.inputAmount}" onchange="handleOnChangeText(event,'inputAmount')"  type="text" name="amount" placeholder="miktar"/>
+    ${UserSelectList({ name: 'receiver',stateControl:"inputTransactionReceiver", hidden:`${state.inputTransactionSender}`})}
+    <button type="button" onclick="javascript:handleTransaction()">Gönder</button>
+    </form>`
+}
+
+const UserSelectList = (props) => {
+  return `<select onchange="javascript:handleOnChangeText(event,'${props.stateControl}')" name="${props.name}">
+    ${state.userList.filter(user=>user.id!==props.hidden)
+      .map(
+        (user) =>
+          '<option ' + (user.id === state[props.stateControl] ? 'selected' : '') + ' value="' + user.id + '" >' + user.adSoyad + '</option>'
+      )
+      .join('')}
+    </select> `
+}
+
+const AddUserForm = () => {
+  return `<form class="addUserForm__form" id="addUserForm">
         <input onchange="handleOnChangeText(event,'inputAdSoyad')" value="${state.inputAdSoyad}" name="adSoyad" type="text" placeholder="Ad Soyad"/>
         <input onchange="handleOnChangeText(event,'inputBakiye')" value="${state.inputBakiye}" name="bakiye" type="text" placeholder="Bakiye"/>
         <button type="button" onclick="javascript:handleAddUser()">Ekle</button>
@@ -41,15 +66,15 @@ const AddUserForm = ()=>{
 }
 
 const UserList = () => {
-    return `
+  return `
     <ul>
-        ${state.userList.map(user=>UserListUser(user)).join("")}
+        ${state.userList.map((user) => UserListUser(user)).join('')}
     </ul>
     `
 }
 
-const UserListUser = (props)=>{
-    return `<li class="userListUser__li">
+const UserListUser = (props) => {
+  return `<li class="userListUser__li">
     <div class="userListUser__text">
     <span>${props.adSoyad}</span>
     <span>${props.bakiye}</span></div>
@@ -58,28 +83,32 @@ const UserListUser = (props)=>{
     </li>`
 }
 
-const render = ()=>{
-    const root = document.getElementById("root")
-    root.innerHTML = App()
+const handleAddUser = () => {
+  //const values = getValuesFromForm("addUserForm")
+  const values = {
+    adSoyad: state.inputAdSoyad,
+    bakiye: Number(state.inputBakiye),
+    id: String(Math.round(Math.random() * 5000)),
+  }
+  state.userList.push(values)
+  state.inputAdSoyad = ''
+  state.inputBakiye = ''
+
+  render()
 }
 
-const handleAddUser = ()=>{
-    //const values = getValuesFromForm("addUserForm")
-    const values = {
-        adSoyad : state.inputAdSoyad,
-        bakiye:state.inputBakiye,
-        id:Math.round(Math.random()*5000)
-     }
-    state.userList.push(values)
-    state.inputAdSoyad = ""
-    state.inputBakiye = ""
+const handleTransaction = ()=>{
+    const sender = state.userList.find(user=>user.id===state.inputTransactionSender)
+    const receiver = state.userList.find(user=>user.id===state.inputTransactionReceiver)
+    if (sender.bakiye<Number(state.inputAmount)){
+        alert("adamda o kadar para yok")
+        return;
+    }
+    sender.bakiye = sender.bakiye - Number(state.inputAmount)
+    receiver.bakiye = receiver.bakiye + Number(state.inputAmount)
+    state.inputAmount = ""
 
     render()
 }
 
-
-
 render()
-
-/* ["a","b"].join(" ") = "a b"
-"a b".split(" ") */
